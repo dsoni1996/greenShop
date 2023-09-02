f0a58481651ff3a79ad3e90b8521750ae57eab6f
Pratik Soni <sonipratiks61@gmail.com>
2023-09-02 10:27:49 +0530

    initial commit

---
+++ green_shop/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import Image from 'next/image'
+import styles from './page.module.css'
+import SlideShow from '@/components/home/SlideShow'
+
+
+export default function Home() {
+  return (
+    <>
+   <SlideShow/>
+    </>
+  )
+}
